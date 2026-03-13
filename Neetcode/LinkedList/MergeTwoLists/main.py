95a7caeb428c52c29b86046a83cdaff032cffb4c
Sean <seancolhoun08@gmail.com>
2026-03-13 12:17:27 +0000

    LinkedList.MergeTwoLists

--- Neetcode/LinkedList/MergeTwoLists/main.py
+++ Neetcode/LinkedList/MergeTwoLists/main.py
@@ -1,56 +1,39 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-"""
-You are given the heads of two sorted linked lists list1 and list2.
-
-Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
-
-The new list should be made up of nodes from list1 and list2.
-"""
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Both lists are already sorted, but they could have a different length.
-        final_node = ListNode()
+        """"
+        dummy = ListNode()
+        tail = dummy
+
         while list1 and list2:
-            if list1.val > list2.val:
-                final_node.val = list2.val
-                final_node.next = list2.next
-                list2 = list2.next
-            elif list2.val > list1.val:
-                final_node.val = list1.val
-                final_node.next = list1.next
+        choose smaller node
+        tail.next = chosen node
+        move chosen list
+        move tail
+
+        attach remaining list
+
+        return dummy.next
+        """
+
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
                 list1 = list1.next
-            else: # equal
-                final_node.val = list1.val
-                final_node.next = list1.next
-                final_node = final_node.next
-
-                final_node.val = list2.val
-                final_node.next = list2.next
-                list1 = list1.next
+            else:
+                tail.next = list2
                 list2 = list2.next
-            
-            final_node = final_node.next
+            tail = tail.next
 
-        if list1 and not list2:
-            while list1:
-                final_node.val = list1.val
-                final_node.next = list1.next
-
-                final_node = final_node.next
+        while list1 or list2:
+            if list1:
+                tail.next = list1
                 list1 = list1.next
-
-        if list2 and not list1:
-            while list2:
-                final_node.val = list1.val
-                final_node.next = list1.next
-
-                final_node = final_node.next
+            else:
+                tail.next = list2
                 list2 = list2.next
-
-        return final_node
+            tail = tail.next
+        return dummy.next
