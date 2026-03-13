18f70443a67fafbbf84416a0f4af1cd7ebab7489
Sean <seancolhoun08@gmail.com>
2026-03-13 12:43:01 +0000

    LinkedList.LinkedListCycleDetection

---
+++ Neetcode/LinkedList/LinkedListCycleDetection/main.py
@@ -0,0 +1,23 @@
+"""
+Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
+
+There is a cycle in a linked list if at least one node in the list can be visited again by following the next pointer. .next = same node
+
+Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle exists.
+
+Note: index is not given to you as a parameter.
+"""
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        return False
+        
