70d5edd1b86bed7e228f3def313702330b8411dc
Sean Colhoun <sean.colhoun@ie.ey.com>
2026-04-10 13:07:28 +0100

    reorderlinkedlist

---
+++ Neetcode/LinkedList/ReorderLinkedList/main.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Given the usual [2,4,6,8]
+
+# Return [2,8,4,6]
+
+
+
+# i.e alternate every second one s.t. x, n, x+1, n-1, ..., whilst x+y < n-m
+
+
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # build it in 2 passes?
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        slow.next = None
+
+        prev = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        first, second = head, prev
+
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
